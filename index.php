4e68c180dbc89fe6c634d952ecdc99796853faf4
Brenoduarte23 <brenoduarte.ti@gmail.com>
2016-09-23 17:21:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>PIBG - Disc&iacute;pulo</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+     <!--link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"-->
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+   <link rel="stylesheet" href="/css/bootstrap.min.css">
+      <link rel="stylesheet" href="/css/font-awesome.min.css">
+      <link rel="stylesheet" href="/css/animate.min.css">
+      <link rel="stylesheet" href="/css/et-line-font.css">
+      <link rel="stylesheet" href="/css/nivo-lightbox.css">
+      <link rel="stylesheet" href="/css/nivo_themes/default/default.css">
+      <link rel="stylesheet" href="/css/style.css">
+      <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500' rel='stylesheet' type='text/css'>
+</head>
+<body data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+<!-- preloader section -->
+<div class="preloader">
+  <div class="sk-spinner sk-spinner-circle">
+       <div class="sk-circle1 sk-circle"></div>
+       <div class="sk-circle2 sk-circle"></div>
+       <div class="sk-circle3 sk-circle"></div>
+       <div class="sk-circle4 sk-circle"></div>
+       <div class="sk-circle5 sk-circle"></div>
+       <div class="sk-circle6 sk-circle"></div>
+       <div class="sk-circle7 sk-circle"></div>
+       <div class="sk-circle8 sk-circle"></div>
+       <div class="sk-circle9 sk-circle"></div>
+       <div class="sk-circle10 sk-circle"></div>
+       <div class="sk-circle11 sk-circle"></div>
+       <div class="sk-circle12 sk-circle"></div>
+    </div>
+</div>
+<body data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+<!-- preloader section -->
+<div class="preloader">
+	<div class="sk-spinner sk-spinner-circle">
+       <div class="sk-circle1 sk-circle"></div>
+       <div class="sk-circle2 sk-circle"></div>
+       <div class="sk-circle3 sk-circle"></div>
+       <div class="sk-circle4 sk-circle"></div>
+       <div class="sk-circle5 sk-circle"></div>
+       <div class="sk-circle6 sk-circle"></div>
+       <div class="sk-circle7 sk-circle"></div>
+       <div class="sk-circle8 sk-circle"></div>
+       <div class="sk-circle9 sk-circle"></div>
+       <div class="sk-circle10 sk-circle"></div>
+       <div class="sk-circle11 sk-circle"></div>
+       <div class="sk-circle12 sk-circle"></div>
+    </div>
+</div>
+
+<!-- navigation section -->
+<section class="navbar navbar-fixed-top custom-navbar" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<p class="navbar-header"">
+				<a href="#" ><img src="/images/logo.png"> </a>
+			</p>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="/" class="smoothScroll">HOME</a></li>
+				
+				<li><a href="/discipulador" class="smoothScroll">DISCIPULADORES</a></li>
+			
+				<li><a href="#contact" class="smoothScroll">CONTATO</a></li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<!-- home section -->
+<section id="home">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<h3>FAÇA PARTE DA NOSSA EQUIPE</h3>
+				<h1>MINISTERIO DE INTEGRAÇÃO</h1>
+				<hr>
+				<a href="/discipulador/" class="smoothScroll btn btn-danger">Quer ser um Discipulador?</a>
+				<a href="#contact" class="smoothScroll btn btn-default">Quero Receber Estudo</a>
+			</div>
+
+		</div>
+	</div>		
+</section>
+
+
+<!-- work section -->
+
+<section id="work">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<div class="section-title">
+					
+					<h1 class="heading bold">HOME</h1>
+					<hr>
+				</div>
+			</div>
+			<a href="/discipulador" >
+			<div class="col-lg-4 col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.3s">
+				<i class="icon-book-open medium-icon"></i>
+					<h3>Ser Discipulador</h3>
+					<hr>
+					<p>Cadastre Como Discipulador e Multiplique o amor de Deus.</p>
+			</div></a>
+			<a href="/ministerio" >
+			<div class="col-lg-4 col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+				<i class="icon-lock medium-icon"></i>
+				<h3>Ministerio</h3>
+				<hr>
+				<p>Esta sessão é permitida somente para o pessoal autorizado.</p>
+				
+			</div></a>
+			<a href="/sistema">
+			<div class="col-lg-4 col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="1s">
+				<i class="icon-lock medium-icon"></i>
+				<h3>Sistema</h3>
+				<hr>
+				<p>Esta sessão é permitida somente para o pessoal autorizado. </p>
+			</div></a>
+			
+		</div>
+	</div>
+</section>
+<!-- contact section -->
+<section id="contact">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 text-center">
+				<div class="section-title">
+					<strong></strong>
+					<h1 class="heading bold">CONTATOS</h1>
+					<hr>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12 contact-info">
+				<h2 class="heading bold">CONTATOS</h2>
+				<p>Deixe-nos seus contatos, para que entremos em contato com você para estudarmos a bíblia juntos!</p>
+				<div class="col-md-6 col-sm-4">
+					<h3><i class="icon-envelope medium-icon wow bounceIn" data-wow-delay="0.6s"></i> EMAIL</h3>
+					<p>Pibg@live. Com</p>
+				</div>
+				<div class="col-md-6 col-sm-4">
+					<h3><i class="icon-phone medium-icon wow bounceIn" data-wow-delay="0.6s"></i> FONE</h3>
+					<p>(84) 9 9120-7185 | (84) 9 9103-6799</p>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<form action="/php/receber.php" method="post" class="wow fadeInUp" data-wow-delay="0.6s">
+					<div class="col-md-6 col-sm-6">
+						<input type="text" class="form-control" placeholder="Name" name="name" required=""/>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<input type="number" class="form-control" placeholder="Contato" name="numero"  required=""/>
+					</div>
+					<div class="col-md-12 col-sm-12">
+						<input tipe="email" class="form-control" placeholder="Email" rows="7" name="email" required="" />
+					</div>
+					<div class="col-md-offset-4 col-md-8 col-sm-offset-4 col-sm-8">
+						<input type="submit" class="form-control" value="Enviar Contatos">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<script src="/js/jquery.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/smoothscroll.js"></script>
+<script src="/js/isotope.js"></script>
+<script src="/js/imagesloaded.min.js"></script>
+<script src="/js/nivo-lightbox.min.js"></script>
+<script src="/js/jquery.backstretch.min.js"></script>
+<script src="/js/wow.min.js"></script>
+<script src="/js/custom.js"></script>
+
+</body>
+</html>
